586891dc86d72a70fdac05b546f0f24ca4434b8e
Your Name <you@example.com>
2019-07-28 23:18:07 -0500

    Composite Design Pattern

---
+++ Composite-Design-Pattern/src/com/design/composite/Shape.java
@@ -0,0 +1,7 @@
+
+package com.design.composite;
+
+public interface Shape {
+	
+	public void draw(String fillColor);
+}
---
+++ Composite-Design-Pattern/src/com/design/composite/Triangle.java
@@ -0,0 +1,12 @@
+
+package com.design.composite;
+
+public class Triangle implements Shape {
+
+	@Override
+	public void draw(String fillColor) {
+		
+		System.out.println("Drawing Triangle with color " + fillColor);
+	}
+
+}
