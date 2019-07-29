a5e0307dc2754d36874b58d44ff955fb7c29c26a
Your Name <you@example.com>
2019-07-29 12:20:05 -0500

    Interpreter Design Pattern

---
+++ Interpreter-Design-Pattern/src/com/design/interpreter/InterpreterContext.java
@@ -0,0 +1,15 @@
+
+package com.design.interpreter;
+
+public class InterpreterContext {
+
+	public String getBinaryFormat(int i){
+		
+		return Integer.toBinaryString(i);
+	}
+	
+	public String getHexadecimalFormat(int i){
+		
+		return Integer.toHexString(i);
+	}
+}
