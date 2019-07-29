79ef39cb562053dc8e03608ae947d245f5d0364b
Your Name <you@example.com>
2019-07-29 00:44:28 -0500

    Proxy Design Pattern

---
+++ Proxy-Design-Pattern/src/com/design/proxy/CommandExecutor.java
@@ -0,0 +1,7 @@
+
+package com.design.proxy;
+
+public interface CommandExecutor {
+
+	public void runCommand(String cmd) throws Exception;
+}
---
+++ State-Design-Pattern/src/com/design/state/TVStopState.java
@@ -0,0 +1,12 @@
+
+package com.design.state;
+
+public class TVStopState implements State {
+
+	@Override
+	public void doAction() {
+		
+		System.out.println("TV is turned OFF");
+	}
+
+}
