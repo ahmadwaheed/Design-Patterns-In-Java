0160da37baad4243ca7ef9ac427ce158be188a4b
Your Name <you@example.com>
2019-07-28 23:24:41 -0500

    Composite Design Pattern

--- Composite-Design-Pattern/src/com/design/composite/Drawing.java
+++ Composite-Design-Pattern/src/com/design/composite/Drawing.java
@@ -20,16 +20,19 @@ public class Drawing implements Shape{
 	
 	//adding shape to drawing
 	public void add(Shape s){
+		
 		this.shapes.add(s);
 	}
 	
 	//removing shape from drawing
 	public void remove(Shape s){
+		
 		shapes.remove(s);
 	}
 	
 	//removing all the shapes
 	public void clear(){
+		
 		System.out.println("Clearing all the shapes from drawing");
 		this.shapes.clear();
 	}
